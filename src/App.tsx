56b8f7edbbddad20ffaa0a899bae634cdcd9f5bd
mohammed.abozamel112 <mohammed.abozamel112@gmail.com>
2025-04-20 12:04:25 +0300

    Add react-helmet-async for improved SEO and meta tags in Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <>
+    <HelmetProvider>
       <Home />
-    </>
+    </HelmetProvider>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,27 @@
-import { ParticlesBackground } from "@/components/ConstellationBackground";
-import HeroScrollDemo from "@/components/container-scroll-animation-demo";
-import { Navbar } from "@/components/navbar-menu-demo";
 import React, { Fragment } from "react";
+import { Helmet } from "react-helmet-async";
+import { ParticlesBackground } from "../components/ConstellationBackground";
+import HeroScrollDemo from "../components/container-scroll-animation-demo";
+import { Navbar } from "../components/navbar-menu-demo";
 
 const Home = () => {
   return (
     <Fragment>
+      <Helmet>
+        <title>Fid Marketing Company - Home</title>
+        <meta name="description" content="Welcome to Fid Marketing Company, your trusted partner for innovative marketing solutions." />
+        <meta property="og:title" content="Fid Marketing Company - Home" />
+        <meta property="og:description" content="Welcome to Fid Marketing Company, your trusted partner for innovative marketing solutions." />
+        <meta property="og:image" content="/src/assets/images/Logo.png" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Fid Marketing Company - Home" />
+        <meta name="twitter:description" content="Welcome to Fid Marketing Company, your trusted partner for innovative marketing solutions." />
+        <meta name="twitter:image" content="/src/assets/images/Logo.png" />
+      </Helmet>
       <ParticlesBackground>
         <Navbar />
         <HeroScrollDemo />
-        
       </ParticlesBackground>
     </Fragment>
   );
